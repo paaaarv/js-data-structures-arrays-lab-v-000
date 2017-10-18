c9315fc2be96ae8ee0eb3a55d4f1d33ffcf9c91c
Parv Kumar <parvathikumar92@gmail.com>
2017-10-18 17:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const drivers = ["Milo", "Otis", "Garfield"]
 
 
 function destructivelyAppendDriver(name){
-  drivers.push("Ralph")
+  drivers.push(name)
 }
+
+function destructivelyPrependDriver(name){}
